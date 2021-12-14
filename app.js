24cb9ec0ae75b4a937ba56913494e5449d8af068
sudoshreyansh <shreyanshjain14072002@gmail.com>
2021-12-14 22:50:45 +0530

    removed mongodb

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI);
-
 app.set('etag', false);
 app.use(bodyParser.json());
 
